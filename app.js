4e8bebe5441325a5f4e7ebb095f3296585a7d379
Sanchit Jain <jainsanchit157@gmail.com>
2019-08-26 00:53:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ class Products {
       products = products.map(item => {
         const { title, price } = item.fields;
         const { id } = item.sys;
-        const image = item.fields.image.fields.file.url;
+        const image = ./product-{id}.jpeg;
         return { title, id, price, image };
       });
       return products;
